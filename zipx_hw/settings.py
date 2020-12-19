0658c80bc43c9294dc90eedc9d0c3b15ee624ed8
pyk <x24870@gmail.com>
2020-12-19 14:41:11 +0800

    add AUTH_USER_MODEL to settings.py

--- zipx_hw/settings.py
+++ zipx_hw/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Custom user model
+AUTH_USER_MODEL = 'users.CustomUser'
